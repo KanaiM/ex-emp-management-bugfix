5c330dd9c7ff6292adfc802637bdb216c64cce33
minami <minami.kanai@rakus-partners.co.jp>
2024-11-22 14:22:04 +0900

    (1-2)管理者登録画面に入力値チェックを追加しました

--- src/main/java/com/example/form/InsertAdministratorForm.java
+++ src/main/java/com/example/form/InsertAdministratorForm.java
@@ -1,5 +1,9 @@
 package com.example.form;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 /**
  * 管理者情報登録時に使用するフォーム.
  * 
@@ -8,10 +12,14 @@ package com.example.form;
  */
 public class InsertAdministratorForm {
 	/** 名前 */
+	@NotBlank(message = "必須入力です")
 	private String name;
 	/** メールアドレス */
+	@NotBlank(message = "必須入力です")
+	@Email(message = "Emailアドレスの形式が不正です")
 	private String mailAddress;
 	/** パスワード */
+	@Size(min = 5, message = "5文字以上で入力してください")
 	private String password;
 
 	public String getName() {
