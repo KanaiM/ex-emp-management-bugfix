86841c0fb3c898ca764e70b9273c1a5a24b35f24
minami <minami.kanai@rakus-partners.co.jp>
2024-11-25 17:28:17 +0900

    (2-1)ログイン者名を従業員一覧と詳細画面で表示するよう変更しました

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -112,6 +112,7 @@ public class AdministratorController {
 			redirectAttributes.addFlashAttribute("errorMessage", "メールアドレスまたはパスワードが不正です。");
 			return "redirect:/";
 		}
+		session.setAttribute("administratorName", administrator);
 		return "redirect:/employee/showList";
 	}
 
