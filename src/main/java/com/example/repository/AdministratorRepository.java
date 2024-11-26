6b8d6456fc75117000f302fb5d6f04190689af89
minami <minami.kanai@rakus-partners.co.jp>
2024-11-26 11:31:23 +0900

    (5-1)SQL Injection対策を行うよう変更しました

--- src/main/java/com/example/repository/AdministratorRepository.java
+++ src/main/java/com/example/repository/AdministratorRepository.java
@@ -59,8 +59,8 @@ public class AdministratorRepository {
 	 */
 	public Administrator findByMailAddressAndPassward(String mailAddress, String password) {
 		String sql = "select id,name,mail_address,password from administrators where mail_address= '" + mailAddress
-				+ "' and password='" + password + "'";
-		SqlParameterSource param = new MapSqlParameterSource();
+				+ "' and password=:password";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("password", password);
 		List<Administrator> administratorList = template.query(sql, param, ADMINISTRATOR_ROW_MAPPER);
 		if (administratorList.size() == 0) {
 			return null;
